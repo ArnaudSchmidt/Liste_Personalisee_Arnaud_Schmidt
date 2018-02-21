e6fc9f210c50882044f0c546c4fea792cd678e95
ArnaudSchmidt <a.schmidt@ludus-academie.com>
2018-02-21 11:50:18 +0100

    Un peu plus de commentaires

--- CustomList_ArnaudSchmidt/app/src/main/java/com/example/schmi/customlist_arnaudschmidt/ChoiceCode.java
+++ CustomList_ArnaudSchmidt/app/src/main/java/com/example/schmi/customlist_arnaudschmidt/ChoiceCode.java
@@ -10,8 +10,10 @@ import static com.example.schmi.customlist_arnaudschmidt.R.id.textView_nom;
 
 public class ChoiceCode extends AppCompatActivity {
 
+    //tableau contenant le texte à afficher après le clic
     String[] DETAIL = {"La balle est grosse.", "Un sport de bonhommes.", "Pour avoir un seul gros bras.", "C'est comme du hand mais dans l'eau."};
 
+    //déclaration de la textview dans laquelle sera mis le texte
     private TextView detail;
 
     @Override
@@ -19,11 +21,13 @@ public class ChoiceCode extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choice);
 
+        //association de l'élément de la vue avec le code
         detail =(TextView)findViewById(R.id.textView_choice);
 
         //récupération des données de l'intent
         Intent intent = getIntent();
 
+        //modification de la valeur du texte en fonction de la donnée stockée dans l'intent
         detail.setText(DETAIL[intent.getIntExtra("Posichoix", 0)]);
 
     }
--- CustomList_ArnaudSchmidt/app/src/main/java/com/example/schmi/customlist_arnaudschmidt/MainActivity.java
+++ CustomList_ArnaudSchmidt/app/src/main/java/com/example/schmi/customlist_arnaudschmidt/MainActivity.java
@@ -18,10 +18,13 @@ import static com.example.schmi.customlist_arnaudschmidt.R.id.listview;
 
 public class MainActivity extends AppCompatActivity {
 
+    //tableau avec les images
     int[] IMAGES = {R.drawable.basket_icon, R.drawable.rugby_icon, R.drawable.tennis_icon, R.drawable.waterpolo_icon};
 
+    //tableau avec les sports
     String[] NOMS = {"Basketball", "Rugby", "Tennis", "Water-Polo"};
 
+    //tableau avec les types de sports
     String[] DESCRIPTIONS = {"Sport collectif", "Sport Collectif", "Sport Individuel", "Sport Collectif"};
 
 
@@ -30,8 +33,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //associe l'objet dans la vue au code
         ListView listView=(ListView)findViewById(listview);
 
+        //instance de la classe customeadapter qui servira à adapter la liste
         CustomAdapter customAdapter = new CustomAdapter();
 
         listView.setAdapter(customAdapter);
@@ -43,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
                 //création de l'intent
                 Intent userIntent = new Intent(MainActivity.this, ChoiceCode.class);
 
+                //on ajoute à l'intent la position de l'item dans la liste sur lequel on a cliqué
                 userIntent.putExtra("Posichoix", position);
 
                 //mène à l'activité suivante
@@ -57,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
     class CustomAdapter extends BaseAdapter{
 
 
+        //retourne la longueur du tableau d'images pour savoir combien de fois répeter les fonctions/procédures
         @Override
         public int getCount() {
             return IMAGES.length;
@@ -72,15 +79,18 @@ public class MainActivity extends AppCompatActivity {
             return 0;
         }
 
+        //modifie la vue selon les données prises dans les tableaux
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
 
             convertView = getLayoutInflater().inflate(R.layout.customlayout,null);
 
+            //associe chaque élément de la liste à un élément du customlayout
             ImageView imageview = (ImageView) convertView.findViewById(imageView);
             TextView textView_nom = (TextView) convertView.findViewById(R.id.textView_nom);
             TextView textView_description = (TextView) convertView.findViewById(R.id.textView_description);
 
+            //transforme les éléments de la liste en fonction de la position de l'élément dans la liste
             imageview.setImageResource(IMAGES[position]);
             textView_nom.setText(NOMS[position]);
             textView_description.setText(DESCRIPTIONS[position]);
